a62c8d85ccb3e4de7ad4d39496a89f92acb32a74
Nytro-maniac25 <tyrongracias@gmail.com>
2025-11-13 16:52:43 +0530

    Fix Vite base path for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Change this to '/' if using a custom domain
-const base = process.env.NODE_ENV === 'production' ? '/tyron-portfolio/' : '/'
-
 export default defineConfig({
   plugins: [react],
-  base: base,
+  base: '/',
 })
